b7a23e6026411321a6ff7fa221fd197387e3ddf5
Damian Oguche <damianguch@yahoo.com>
2023-02-16 13:56:43 -0800

    Correction on 6-size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,17 +8,11 @@
 
 int main(void)
 {
-	char name;
-	int age;
-	long number;
-	long long large;
-	float amount;
-
-	printf("Size of a char: %lu bytes(s)\n", sizeof(name));
-	printf("Size of a int: %lu bytes(s)\n", sizeof(age));
-	printf("Size of a long int: %lu bytes(s)\n", sizeof(number));
-	printf("Size of a long long int: %lu bytes(s)\n", sizeof(large));
-	printf("Size of a float: %lu bytes(s)\n", sizeof(amount));
+	printf("Size of a char: %lu bytes(s)\n", sizeof(char));
+	printf("Size of a int: %lu bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu bytes(s)\n", sizeof(float));
 
 	return (0);
 }
