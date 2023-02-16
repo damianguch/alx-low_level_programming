bbf92ccfea1f1276c42141c97ecb7aa951233c07
Damian Oguche <damianguch@yahoo.com>
2023-02-16 07:31:49 -0800

    6-size.c file created

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d bytes(s)\n", sizeof(char);
+	printf("Size of a int: %d bytes(s)\n", sizeof(int);
+	printf("Size of a long: %d bytes(s)\n", sizeof(long);
+	printf("Size of a long long: %d bytes(s)\n", sizeof(long long);
+	printf("Size of a float: %d bytes(s)\n", sizeof(float);
+	return (0);
+}
