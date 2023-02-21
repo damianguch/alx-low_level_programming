5195b0b87925a9fad1a3ee0fc88337b047d52e34
Damian Oguche <damianguch@yahoo.com>
2023-02-21 07:04:14 -0800

    2-print_alphabet_x10.c file created

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -  Prints the letters of the alphabets 10 times.
+ *
+ * Description: Function to print the lowercase alphabets ten times
+ *
+ * Return: Returns void.
+ */
+
+
+void print_alphabet_x10(void);
+{
+	char c;
+	int i;
+
+	for (i = 0; 1 < 10; i++)
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+	_putchar('\n);
+}
+
