ac9b4b76e73fda60cc2da0c696082b27d8e38b8d
Damian Oguche <damianguch@yahoo.com>
2023-02-21 03:43:45 -0800

    Create 1-alphabet.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - Function to print the alphabets in lowercase
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a', c <= 'z'; c++;)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+
+}
