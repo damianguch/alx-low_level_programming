822811264d9d92c049f41417a006276d64f72f98
Damian Oguche <damianguch@yahoo.com>
2023-02-23 08:50:31 -0800

    10-print_triangle.c file created

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle, using the character #.
+ * @size: The size of the triangle.
+ */
+
+void print_triangle(int size)
+{
+	int row, col;
+
+	if (size > 0)
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (col = size - row; col > 0; col--)
+				_putchar(' ');
+
+			for (col = 0; col < row; col++)
+				_putchar('#');
+
+			if (row == size)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
