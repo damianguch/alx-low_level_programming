8754f1b59978f5f2f290a9b271ca1f28cccb2ce8
Damian Oguche <damianguch@yahoo.com>
2023-03-06 03:14:10 -0800

    2-strchr.c file corrected

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,14 +5,14 @@
  * @s: the string
  * @c: the character
  *
- * Return: a pointer to a character aka the character found
+ * Return: a pointer to the character or NULL
  */
 
 char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0' ; i++)
+	for (i = 0; s[i] >= '\0' ; i++)
 	{
 		if (s[i] == c)
 		return (s + i);
