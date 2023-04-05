be22a2766a22255f403d9b9e7d6582aa1faf7228
Damian Oguche <damianguch@yahoo.com>
2023-04-05 10:15:59 -0700

    7-get_nodeint.c file modified

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,9 +18,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while ((k < index) && head != NULL)
 	{
 		head = head->next;
-		i++;
+		k++;
 	}
 
 	if (k == index)
 		return (head);
+
+	return (NULL);
 }
