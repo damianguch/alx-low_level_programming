6ca64723574916671b1ecc0fcd94d2014ee686e5
Damian Oguche <damianguch@yahoo.com>
2023-04-05 11:56:35 -0700

    10-delete_nodeint.c file modified

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while (i < (index - 1) && tmp != NULL)
+	while (k < (index - 1) && t != NULL)
 	{
 		t = t->next;
 		k++;
