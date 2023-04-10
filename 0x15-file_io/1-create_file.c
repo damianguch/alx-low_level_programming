2cab319277434416e384e95fdd7494eefe131461
Damian Oguche <damianguch@yahoo.com>
2023-04-10 08:58:38 -0700

    1-create_file.c file modified

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,13 +16,13 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
 	if (text_content == NULL)
 	{
 		close(fd);
 		return (1);
 	}
-
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	num_bytes = sizeof(text_content);
 
 	bytes_written = write(fd, text_content, num_bytes);
