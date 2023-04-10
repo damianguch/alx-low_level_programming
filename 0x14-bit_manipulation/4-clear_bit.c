189475ce3a37ef077dfbed65a9faf399e3e00bb9
Damian Oguche <damianguch@yahoo.com>
2023-04-10 16:33:48 -0700

    4-clear_bit.c file modified

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,7 +13,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	*n = *n & ~(1 << index);
 
 	if (index > 8 * sizeof(*n))
-		return(-1);
+		return (-1);
 	else
-		return(1);
+		return (1);
 }
