2281515bf319b3602fb9e2ba5a62dcbea0e5f764
Damian Oguche <damianguch@yahoo.com>
2023-05-19 09:31:12 -0700

    5-get_dnodeint.c file modified

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,7 +10,7 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i;
-	for (i = 0; i <= index; i++)
+	for (i = 0; i = index; i++)
 	{
 		if (head == NULL)
 			return (NULL);
